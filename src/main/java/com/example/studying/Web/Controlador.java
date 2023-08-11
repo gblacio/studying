c93343f445c1eadff7ce345e7005d5501baf8c0c
Giuseppe Blacio <giuseppe.blacio@gmail.com>
2023-08-10 22:29:40 -0700

    v3

--- src/main/java/com/example/studying/Web/Controlador.java
+++ src/main/java/com/example/studying/Web/Controlador.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Controlador {
@@ -23,6 +24,25 @@ public class Controlador {
     public String addCustomer(Model model){
         model.addAttribute("cliente", new Cliente());
         return "modify";
+    }
+
+    @PostMapping(path = "/save")
+    public String saveCustomer(Cliente cliente){
+        dao.save(cliente);
+        return "redirect:/";
+    }
+
+    @GetMapping(path = "/edit/{idcliente}")
+    public String editCustomer(Cliente cliente, Model model){
+        cliente = dao.findById(cliente.getIdcliente()).orElse(null);;
+        model.addAttribute("cliente", cliente);
+        return "modify";
+    }
+
+    @GetMapping(path = "/delete")
+    public String deleteCustomer(Cliente cliente){
+        dao.delete(cliente);
+        return "redirect:/";
     }
 
 }
